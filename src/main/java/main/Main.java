43f5cd0e548350b592450789e208fe78b1b0783c
Mateo <158166080+Mateo-Padilla@users.noreply.github.com>
2025-09-15 09:35:06 -0300

    update: Alterando "Main".

--- src/main/java/main/Main.java
+++ src/main/java/main/Main.java
@@ -3,8 +3,11 @@
  */
 package main;
 
-import java.net.*;
-import java.io.*;
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.io.PrintWriter;
+import java.net.Socket;
 import javax.swing.JOptionPane;
 
 /**
